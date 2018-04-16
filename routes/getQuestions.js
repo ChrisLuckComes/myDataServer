87fd5a85e9d59bbed57f505936107305b9b9cb86
ChrisLuckComes <34127629+ChrisLuckComes@users.noreply.github.com>
2018-04-16 23:06:04 +0800

    修改接口 一次返回所有collection

--- routes/getQuestions.js
+++ routes/getQuestions.js
@@ -1,21 +1,23 @@
-var express = require('express')  
+var express = require('express')
 var router = express.Router();
 
 const url = require('url')
 const MongoClient = require('mongodb').MongoClient
 const murl = "mongodb://127.0.0.1:27017"
 
-router.get('/',(req,res,next)=>{
-    var key = url.parse(req.url, true).query.key
-    console.log(`试卷编号：${key}`)
-    MongoClient.connect(murl,(err,client)=>{
-        var col=client.db('questions').collection('test')
-        col.find({test:+key}).toArray((err,arr)=>{
-            console.log(arr)
-            client.close()
-            res.send(arr)                            
-        })         
-    }) 
+router.get('/', (req, res, next) => {
+    MongoClient.connect(murl, (err, client) => {
+        // var col=client.db('questions').collection(key)
+        ~async function getTest(params) {
+            let ret = await client.db('questions').collections()
+            Promise.all(ret.map(item =>
+                item.find({}).toArray()
+            )).then(ret => {
+                res.send(ret)
+                client.close()
+            })
+        }()
+    })
 })
 
 module.exports = router
